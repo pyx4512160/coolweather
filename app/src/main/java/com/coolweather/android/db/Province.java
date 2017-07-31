e7625367b73b232eb87c9bd19513fa06197d5b19
Lang <674298881@qq.com>
2017-08-01 01:54:49 +0800

    完成遍历省市县三级列表功能

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -6,8 +6,8 @@ import org.litepal.crud.DataSupport;
  * Created by Administrator on 2017/7/29.
  */
 
+private int id;
 public class Province extends DataSupport {
-    private int id;
     private String provinceName;
     private int provinceCode;
     public int getId(){
