0d0571d1cf96420571530d7f44cefcb77d30e914
Lang <674298881@qq.com>
2017-08-04 22:35:38 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
-package com.coolweather.android.db;
+package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,6 +16,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coolweather.android.R;
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
+import com.coolweather.android.db.Province;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -80,6 +84,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -2,12 +2,13 @@ package com.coolweather.android.db;
 
 import org.litepal.crud.DataSupport;
 
+
 /**
  * Created by Administrator on 2017/7/29.
  */
 
-private int id;
 public class Province extends DataSupport {
+    private int id;
     private String provinceName;
     private int provinceCode;
     public int getId(){
